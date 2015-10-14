7183eefba28ca3b769f7a4cf0f88cd47563be11e
Hoosk <hoosksky@gmail.com>
2015-10-14 10:49:23 +0200

    Fixed Routes

--- src/BlogLaravel/Http/routes.php
+++ src/BlogLaravel/Http/routes.php
@@ -3,18 +3,16 @@
 // SHOW POSTS
 
 
-Route::group(['as' => 'blog'], function () {
+Route::group(['prefix' => 'blog'], function () {
   get('/', 'BlogController@index'); // Get all posts
   get('category/{id}', 'BlogController@category'); // Get all posts by category
   get('post/{id}', 'BlogController@show'); //View a post
-  // CREATE COMMENTS OF POSTS AND COMMENTS
+
   post('post/{id}', 'ComentariController@comentaPost'); // Create comment about post
   post('comentario/{id}', 'ComentarioController@comentaComentario'); // Create comment about comment
 });
 
 //Admin Backend
-Route::group(['as' => 'admin'], function () {
+Route::group(['prefix' => 'admin'], function () {
   Route::resource('post', 'PostController');
-
-
 });
